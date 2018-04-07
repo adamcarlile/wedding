ed78f20cd2b7f9111baf2d88314af359c9ff658f
Adam Carlile <github@adamcarlile.com>
2018-04-07 22:15:30 +0100

    remove master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  config.require_master_key = true
   
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
