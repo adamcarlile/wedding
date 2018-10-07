986def01ea1026728605027736296b575f9ca6a8
Adam Carlile <hello@adamcarlile.com>
2018-10-07 20:51:14 +0100

    WIP: RSVP

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -4,6 +4,9 @@ class Invitee < ApplicationRecord
   has_many :communication_deliveries, class_name: 'Communications::Delivery', dependent: :destroy
   has_many :communications, through: :communication_deliveries
 
+  has_many :attendances
+  has_many :events, through: :attendances
+
   validates :firstname, presence: true
   validates :lastname, presence: true
 
@@ -14,4 +17,17 @@ class Invitee < ApplicationRecord
   def name
     [firstname, lastname].join(' ')
   end
+
+  def attend!(event)
+    attendances.find_or_create_by(event: event)
+  end
+
+  def decline!(event)
+    attendances.find_by(event: event)&.destroy
+  end
+
+  def attending?(event)
+    attendances.find_by(event: event)
+  end
+
 end
