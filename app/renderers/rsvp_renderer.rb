b6042be2ad5f3c815a40df58022f127ca7eafdc2
Adam Carlile <hello@adamcarlile.com>
2018-10-10 00:37:46 +0100

    Remove crash

--- app/renderers/rsvp_renderer.rb
+++ app/renderers/rsvp_renderer.rb
@@ -1,23 +1,23 @@
-class EventRenderer
-  attr_accessor :options, :type
-  attr_writer :content
-  def initialize(event, party, context, options = {})
-    @event    = event
-    @party    = party
-    @context  = context
-    @options  = options
-  end
+# class EventRenderer
+#   attr_accessor :options, :type
+#   attr_writer :content
+#   def initialize(event, party, context, options = {})
+#     @event    = event
+#     @party    = party
+#     @context  = context
+#     @options  = options
+#   end
 
-  def render
-    @context.content_tag(:div, class: 'rsvp')
+#   def render
+#     @context.content_tag(:div, class: 'rsvp')
 
-  # def render
-  #   @context.content_tag(:div, class: 'event') do
-  #     [render_header, render_content].compact.join("\n").html_safe
-  #   end.html_safe
-  # end
+#   # def render
+#   #   @context.content_tag(:div, class: 'event') do
+#   #     [render_header, render_content].compact.join("\n").html_safe
+#   #   end.html_safe
+#   # end
 
-  private
+#   private
 
 
-end
+# end
