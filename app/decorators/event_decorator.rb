feb8410b505e507658a002c060534107655dbb63
Adam Carlile <hello@adamcarlile.com>
2019-01-13 20:56:26 +0000

    Fix bug

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,11 +2,11 @@ class EventDecorator < ApplicationDecorator
   delegate_all
   
   def attending
-    attendances.joins(:invitee).attending.order.(:created_at).order('invitees.party_id')
+    attendances.joins(:invitee).attending.order(created_at: :desc).order('invitees.party_id')
   end
 
   def declined
-    attendances.joins(:invitee).declined.order(:created_at).order('invitees.party_id')
+    attendances.joins(:invitee).declined.order(created_at: :desc).order('invitees.party_id')
   end
 
   def undecided
